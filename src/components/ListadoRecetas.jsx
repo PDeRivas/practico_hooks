38b1fe2e5f5b86e2e2b687d92defb0bda3576816
Pablo de Rivas <p.derivas@itecriocuarto.org.ar>
2024-08-28 22:49:02 -0300

    Pequeño arreglo a titulo del listado

--- src/components/ListadoRecetas.jsx
+++ src/components/ListadoRecetas.jsx
@@ -6,7 +6,7 @@ export default function ListadoRecetas() {
     let {recetas, setRecetas} = useContext(ContextoRecetas)
     return (
     <div className='flex flex-col items-center col-span-4'>
-        <h1>Mis recetas...</h1>
+        <h1>Mis recetas</h1>
         {recetas.map((receta) =>{
             return(<TarjetaReceta nombre={receta.nombre} descripcion={receta.descripcion} id={receta.id}/>)
         })}
